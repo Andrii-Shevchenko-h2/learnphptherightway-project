7a1396d1b212beba5dd52dc102fe941cbe013ccb
Andrii-Shevchenko-h2 <219667769+Andrii-Shevchenko-h2@users.noreply.github.com>
2025-08-22 08:43:26 +0200

    feat: add income, expenses and total to table and format their colors

--- vendor/csvTable.php
+++ vendor/csvTable.php
@@ -6,19 +6,24 @@ $getSecureString = fn(string $str): string => htmlspecialchars($str, ENT_QUOTES)
 
 $generateTableCaption = fn(string $name): string => '<caption>'. $getSecureString($name) .'</caption>';
 
-$formatDate = fn(string $date): string => date('D, \t\h\e jS F, Y', strtotime($getSecureString($date)));
+$formatDate = fn(string $date): string => date('D, \t\h\e jS F, Y', strtotime($date));
 
-$formatAmountColor = fn(string $amount): string => $amount[0] === '-' ? '<span style="color: red">'. $getSecureString($amount) .'</span>' : '<span style="color: green">'. $getSecureString($amount) .'</span>';
+$formatAmountColor = fn(string $amount): string => $amount[0] === '-' ? '<span style="color: red">'. $amount .'</span>' : '<span style="color: green">'. $amount .'</span>';
 
-$generateTableCell = function(array $row) use ($formatDate, $formatAmountColor, $getSecureString): string {
+$generateTableCell = function(array $row, float &$incomeTotal, float &$expensesTotal) use ($formatDate, $formatAmountColor, $getSecureString): string {
   $string = '';
 
     foreach ($row as $name => $data) {
+    $name = trim($name);
 
     if ($name === 'Date') {
-      $string .= '<td>'. $formatDate($data) .'</td>';
-    } elseif ($name === "Amount\n") {
-      $string .= '<td>'. $formatAmountColor($data) .'</td>';
+      $string .= '<td>'. $formatDate($getSecureString($data)) .'</td>';
+    } elseif ($name === "Amount") {
+      $numericData = (double) str_replace(['$', ','], '', $data);
+      $string .= '<td>'. $formatAmountColor($getSecureString($data)) .'</td>';
+
+      if ($numericData >= 0) $incomeTotal += $numericData;
+      else $expensesTotal += (double) $numericData;
     } else {
       $string .= '<td>'. $getSecureString($data) .'</td>';
     }
@@ -39,29 +44,60 @@ $generateTableHead = function(array $headerRow) use ($getSecureString): string {
   return $ths;
 };
 
-$generateTableBody = function(array $tableData) use ($generateTableCell): string {
+$generateTableBody = function(array $tableData, float &$income, float &$expenses) use ($generateTableCell): string {
   $tableBody = '';
 
   foreach ($tableData as $row) {
-    $tableBody .= '<tr>'. $generateTableCell($row) .'</tr>';
+    $tableBody .= '<tr>'. $generateTableCell($row, $income, $expenses) .'</tr>';
   }
 
   return $tableBody;
 };
 
+$generateStandaloneRow = function(string $valueName, string $value, int $totalCells = 4): string {
+  // current logic for this is not ideal, but who cares
+  $totalCellsString = '';
+
+  for ($i = 0; $i < $totalCells - 1; $i++) {
+    $totalCellsString .= "<td>\n</td>\n";
+  }
+
+  return <<<HTML
+    <tr>
+      $totalCellsString
+      <td>
+        $valueName: $value
+      </td>
+    </tr>
+  HTML;
+};
+
 // will return csv table html string which I can put in later with parse.php
-$generateCsvTable = function(string $csvName, array $csvContent) use ($generateTableHead, $generateTableBody, $generateTableCaption): string {
+$generateCsvTable = function(string $csvName, array $csvContent) use ($generateTableHead, $generateTableBody, $generateTableCaption, $formatAmountColor, $generateStandaloneRow): string {
   $header = $csvContent[0];
+  $incomeTotal = 0;
+  $expensesTotal = 0;
   $csvBodyContent = array_slice($csvContent, 1);
   $tableCaption = $generateTableCaption($csvName);
   $tableHeader = $generateTableHead($header);
-  $tableBody = $generateTableBody($csvBodyContent);
+  $tableBody = $generateTableBody($csvBodyContent, $incomeTotal, $expensesTotal);
+  $total = (string) ($incomeTotal + $expensesTotal);
+  $incomeTotal = (string) $incomeTotal;
+  $expensesTotal = (string) $expensesTotal;
+  $tableColumns = substr_count($tableHeader, '<th>');
+
+  $formattedIncome = $generateStandaloneRow('Income', $formatAmountColor($incomeTotal), $tableColumns);
+  $formattedExpenses = $generateStandaloneRow('Expenses', $formatAmountColor($expensesTotal), $tableColumns);
+  $formattedTotal = $generateStandaloneRow('Total', $formatAmountColor($total), $tableColumns);
 
   return <<<tableHTML
     <table>
       $tableCaption
       $tableHeader
       $tableBody
+      $formattedIncome
+      $formattedExpenses
+      $formattedTotal
     </table>
   tableHTML;
 };
